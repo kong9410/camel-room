62427e3fccb503452b7c33c707fc102d1f1101a4
KONG <37204770+kong9410@users.noreply.github.com>
2019-04-18 01:43:59 +0900

    app_test

--- app_camel_room/sample.js
+++ app_camel_room/sample.js
@@ -1,14 +1,17 @@
-const http = require('http');
-
-const hostname = '127.0.0.1';
-const port = 8000;
-
-const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello World\n');
+var http = require('http');
+var fs = require('fs');
+var app = http.createServer(function(request,response){
+    var url = request.url;
+    if(request.url == '/'){
+      url = '/index.html';
+    }
+    if(request.url == '/favicon.ico'){
+        response.writeHead(404);
+        response.end();
+        return;
+    }
+    response.writeHead(200);
+    response.end(fs.readFileSync(__dirname + url));
+ 
 });
-
-server.listen(port, hostname, () => {
-    console.log('Server running at http://${hostname}:${port}/');
-});
+app.listen(80);
