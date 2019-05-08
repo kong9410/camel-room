3f4a466113f31e02ad2be0060299fc0368d5389a
vkxmfkdl <37204852+vkxmfkdl@users.noreply.github.com>
2019-05-08 13:50:50 +0900

    park works

--- app_camel_room/routes/index.js
+++ app_camel_room/routes/index.js
@@ -1,14 +1,22 @@
 var fs = require('fs');  //file load
 var express = require('express');
 var router = express.Router();
-
+var mongoose    = require('mongoose');
+var db = mongoose.connection;
+mongoose.connect('mongodb://localhost/estate_db',{useNewUrlParser : true});
 
 router.get('/', function(req, res){
-		if(req.session.email){
-			res.render('view_estate.ejs',{check_ses: req.session.email} );
-		}
-		else
-			res.render('view_estate.ejs', {check_ses: 0});
+		var cursor = db.collection("estates").find({}).toArray(function(err,result){			
+			if(err)throw err;
+			console.log(result);
+			if(req.session.email){
+				
+				res.render('view_estate.ejs',{check_ses: req.session.email, estate_list: result} );
+			}
+			else{
+				res.render('view_estate.ejs', {check_ses: 0, estate_list: result});		
+			}		
+		});
 });
 	
 	
